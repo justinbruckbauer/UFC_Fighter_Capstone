3f002c21d0d4f163ae0b9b5f1441b83ba991aff1
justinbruckbauer <bruckbauerjustin@gmail.com>
2025-05-05 14:34:56 -0400

    Add files via upload

---
+++ dump.sqlite.sql
@@ -0,0 +1,9 @@
+-- TABLE
+CREATE TABLE 'ufc_data_final' ('name' TEXT,'wins' INTEGER,'losses' INTEGER,'draws' INTEGER,'height_cm' REAL,'weight_in_kg' REAL,'reach_in_cm' REAL,'stance' TEXT,'significant_strikes_landed_per_minute' REAL,'significant_striking_accuracy' REAL,'significant_strikes_absorbed_per_minute' REAL,'significant_strike_defence' REAL,'average_takedowns_landed_per_15_minutes' REAL,'takedown_accuracy' REAL,'takedown_defense' REAL,'average_submissions_attempted_per_15_minutes' REAL,'age' INTEGER,'strike_differential' TEXT,'win_percentage' TEXT,'weight_class' TEXT);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
